c51a86a89fa4ed105dc87a25aedf310111caf534
Michael Allan <michael@intrinseca.me.uk>
2013-01-20 17:20:13 +0000

    net: add sample protocols
    phidgetsource: basic implementation

--- net/__init__.py
+++ net/__init__.py
@@ -1,51 +1,44 @@
 from twisted.internet import protocol, reactor
 from twisted.internet.protocol import Factory
-from twisted.protocols.basic import LineReceiver, Int32StringReceiver
+from twisted.protocols.basic import Int32StringReceiver
 from twisted.internet.error import ConnectionDone
 
-from protobuf import network_pb2
-
-class SamplesProtocol(LineReceiver):
-  def __init__(self):
-    self.setRawMode()
-  
-  def rawDataReceived(self, data):
-    #self.transport.write(data)
-    print(data)
-        
-  def sendData(self, data):
-    print("SamplesProtocol: Sending {0}" % data)
-    self.transport.write(data)
-  
-  def connectionMade(self):
-    print("SamplesProtocol: Connected")
-
-class SamplesFactory(Factory):
-    def buildProtocol(self, addr):
-        return SamplesProtocol()
-
-class SamplesServerFactory(protocol.ServerFactory):
-    protocol = SamplesProtocol
+from protobuf import network_pb2, samples_pb2
 
 class ProtobufProtocol(Int32StringReceiver):
-  def sendMessage(self, message):
-    print("ProtobufProtocol: Sending")
-    self.sendString(message.SerializeToString())
-    print("ProtobufProtocol: Sent")
+    def sendMessage(self, message):
+        print("ProtobufProtocol: Sending")
+        self.sendString(message.SerializeToString())
+        print("ProtobufProtocol: Sent")
   
-  def stringReceived(self, data):
-    print("ProtobufProtocol: Received")
-    message = network_pb2.network_message()
-    message.ParseFromString(data)
-    print(message)
+    def stringReceived(self, data):
+        print("ProtobufProtocol: Received")
+        message = network_pb2.network_message()
+        message.ParseFromString(data)
+        print(message)
   
-  def connectionMade(self):
-    print("ProtobufProtocol: Connected")
+    def connectionMade(self):
+        print("ProtobufProtocol: Connected")
     
-  def connectionLost(self, reason=ConnectionDone):
-    print("ProtobufProtocol: Connection Lost")
-    print(reason)
+    def connectionLost(self, reason=ConnectionDone):
+        print("ProtobufProtocol: Connection Lost")
+        print(reason)
     
 class ProtobufProtocolFactory(Factory):
-      def buildProtocol(self, addr):
+    def buildProtocol(self, addr):
         return ProtobufProtocol()
+
+class SampleStreamProtocol(ProtobufProtocol):
+    def sendSample(self, timestamp, value):
+        s = samples_pb2.Sample()
+        s.timestamp = timestamp
+        s.value = value
+        
+        m = network_pb2.network_message()
+        m.sample.timestamp = timestamp
+        m.sample.value = value
+        self.sendMessage(m)
+
+class SampleStreamProtocolFactory(Factory):
+    def buildProtocol(self, addr):
+        return SampleStreamProtocol()
--- phidgetsource/__init__.py
+++ phidgetsource/__init__.py
@@ -1,25 +1,81 @@
 from protobuf import samples_pb2, network_pb2
-from twisted.internet import reactor
+from twisted.internet import interfaces, reactor
 from twisted.internet.endpoints import TCP4ClientEndpoint
-from net import ProtobufProtocolFactory
+from net import SampleStreamProtocol, SampleStreamProtocolFactory
 from twisted.python import log
+from zope.interface import implements
+from Phidgets.Devices.InterfaceKit import InterfaceKit
+from Phidgets import PhidgetException
+from twisted.internet.task import LoopingCall
+import time
+from twisted.internet.protocol import Factory
 
-s = samples_pb2.Sample()
-s.timestamp = 10
-s.value = 1024
+class PhidgetSourceProtocol(SampleStreamProtocol):
+    implements(interfaces.IPushProducer)
+    
+    def connectionMade(self):
+        SampleStreamProtocol.connectionMade(self)
+        
+        self._paused = False
+        
+        try:
+            self._device = InterfaceKit()
+        except RuntimeError as e:
+            print("Runtime Error: %s" % e.message)
+            
+        try:
+            self._device.openPhidget()
+        except PhidgetException as e:
+            print("Phidget Exception %i: %s" % (e.code, e.detail))
+        
+        self._sampleLoop = LoopingCall(self.takeSample)
+        print("Phidget: Waiting for Connection")
+        self._device.waitForAttach(10000)
+        print("Phidget: Connected")
+        self.transport.registerProducer(self, True)
+        self.resumeProducing()
+        
+    def pauseProducing(self):
+        self._paused = True
+        self._sampleLoop.stop()
+        
+    def resumeProducing(self):
+        self._paused = False
+        self._sampleLoop.start(1)
+        
+    def stopProducing(self):
+        self._paused = True
+        self._sampleLoop.stop()
+        self._device.closePhidget()
+    
+    def takeSample(self):
+        value = self._device.getSensorValue(0)
+        
+        self.sendSample(int(time.time()), value)
 
-print(s)
+class PhidgetSourceProtocolFactory(Factory):
+    def buildProtocol(self, addr):
+        return PhidgetSourceProtocol()     
 
-def gotProtocol(p):
-    m = network_pb2.network_message()
-    m.sample.CopyFrom(s)
-    p.sendMessage(m)
-    p.transport.loseConnection()
-
-point = TCP4ClientEndpoint(reactor, "localhost", 1234)
-d = point.connect(ProtobufProtocolFactory())
-d.addCallback(gotProtocol)
-d.addErrback(log.err)
+reactor.listenTCP(1234, PhidgetSourceProtocolFactory())
 reactor.run()
 
-print("Done")
+#s = samples_pb2.Sample()
+#s.timestamp = 10
+#s.value = 1024
+#
+#print(s)
+#
+#def gotProtocol(p):
+#    m = network_pb2.network_message()
+#    m.sample.CopyFrom(s)
+#    p.sendMessage(m)
+#    p.transport.loseConnection()
+#
+#point = TCP4ClientEndpoint(reactor, "localhost", 1234)
+#d = point.connect(ProtobufProtocolFactory())
+#reactor.run()
+#d.addCallback(gotProtocol)
+#d.addErrback(log.err)
+#
+#print("Done")
