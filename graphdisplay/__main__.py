c769a2c8cafabd197cba99b5ff04bbd6ca3d7788
Michael Allan <michael@intrinseca.me.uk>
2013-01-31 21:14:50 +0000

    graphdisplay: show new session format

--- graphdisplay/__main__.py
+++ graphdisplay/__main__.py
@@ -1,11 +1,32 @@
 from protobuf import samples_pb2
-import pylab
 from protobuf.samples_pb2 import sample_stream
+import os
+import pylab
 
-data = open("data.pro", "r")
+#get session list
+sessions = os.listdir("storage")
 
-stream = sample_stream()
-stream.ParseFromString(data.read())
+print("Available Sessions:")
+i = 1
+for folder in sessions:
+    print("  %d. %s" % (i, folder))
+    i += 1
 
-pylab.plot(stream.sample)
+#session chooser
+session = sessions[0]
+blocks = os.listdir(os.path.join("storage", session))
+
+blocks = sorted(blocks, key = lambda x: int(x))
+
+samples = []
+
+for block in blocks:
+    print(block)
+    block_file = open(os.path.join("storage", session, block), "r")
+    stream = sample_stream()
+    stream.ParseFromString(block_file.read())
+    samples.extend(stream.sample)
+    
+pylab.plot(samples)
+pylab.grid()
 pylab.show()
