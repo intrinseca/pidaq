e251ffb717427e342bac6f6ddee9351cf17b2d73
Michael Allan <michael@intrinseca.me.uk>
2013-03-04 14:19:25 +0000

    storageengine: use new module for interface

--- sources/spi.py
+++ sources/spi.py
@@ -1,12 +1,12 @@
 from sources import Source
 from twisted.internet import reactor
-from spipy import SPI
+from pidaqif import PiDAQ
 
 class SPISource(Source):
     def __init__(self):
         print("SPI Source")
         Source.__init__(self)
-        self._spi = SPI(0, 0)
+        self._pidaq = PiDAQ(0, 0)
         #self._timer = LoopingCall(self.sample)
         #print("Starting Sample Timer")
         #self._timer.start(1)
@@ -14,18 +14,11 @@ class SPISource(Source):
         reactor.callInThread(self.sample)
         
     def sample(self):
-        skip = 0
         while not self.stopping:
-            length, = self._spi.transfer([], 1)
-            if length > 0:
-                data = self._spi.transfer([], length)
-                print("{:3}->data ({}): {:3} ({:3}) {:3}".format(skip, length, data[0], data[len(data) - 1] - data[0], data[len(data) - 1]))
-                if self.sink is not None:
-                    self.sink(data)
-                    
-                skip = 0
-                
-            skip += 1
+            data = self._pidaq.get_samples()
+            print("data ({}): {:3} ({:3}) {:3}".format(len(data), data[0], data[len(data) - 1] - data[0], data[len(data) - 1]))
+            if self.sink is not None:
+                self.sink(data)
 
     def stop(self):
         self.stopping = True
