eaec06f1160e839899ca5bf4144b40c5c15f96a5
Michael Allan <michael@intrinseca.me.uk>
2013-05-28 15:50:49 +0100

    display all four channels in graphdisplay

--- graphdisplay/__main__.py
+++ graphdisplay/__main__.py
@@ -1,10 +1,10 @@
 from graphdisplay import plot_samples
-from storageengine.storage import Session
+from storageengine.storage import Session, BlockPool
 import os
-
+import pylab
 
 #get session list
-sessions = os.listdir("storage")
+sessions = [s for s in os.listdir("storage") if s.startswith('index-')]
 
 print("Available Sessions:")
 i = 1
@@ -13,12 +13,19 @@ for folder in sessions:
     i += 1
 
 #session chooser
-session_dir = sessions[0]
+session_index = sessions[2]
 
-session_index = open(os.path.join("storage", session_dir, "index"), "rb")
-session = Session.deserialise(session_index.read())
-session_index.close()
+path = os.path.join(os.getcwd(), "storage")
+session_index_file = open(os.path.join(path, session_index), "rb")
+session = Session.deserialise(session_index_file.read())
+session.block_pool = BlockPool(file_root=path, pool_size=10)
+session_index_file.close()
 
-samples = session.query(start=25, end=125)
+pylab.hold(True)
 
-plot_samples(samples)
+for i in range(0,4):
+    (true_start, samples) = session.query(channel=i)
+    pylab.plot(samples)
+    
+pylab.grid()
+pylab.show()
